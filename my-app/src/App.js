769d2400ead17747ce2b3d8ce24f78d25c8b5f18
Ayush Mehra <ayushmehra998@gmail.com>
2023-10-24 03:13:55 +0530

    TodoList Complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,167 @@
-import React, { useEffect } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 import './App.css';
-import { getdata, todoSelector } from './features/counter/todoSlice';
-import { useDispatch, useSelector } from 'react-redux';
 import { Tasklist } from './pages/tasklist';
 
 function App() {
-  const dispatch = useDispatch();
+  const [data,setdata] = useState([]);
+  const input = createRef();
+  const [updatetaskId,setUpdateTaskId]=useState(0);
+  const [updatetaskdata,setUpdateTaskData] = useState({});
+  const [isEdit,setEdit]=useState(false);
+  const [totalTask,setTotalTask]= useState(0);
+  const [completedTask,setCompletedTask] = useState(0);
+  const [pendingTask,setPendingTask] = useState(0);
+  const [filter,setFilter] = useState("All");
   useEffect(()=>{
-    dispatch(getdata());
+    fetchdata();
   },[]);
+  useEffect(()=>{
+    handleCalculate();
+  },[data])
+
+  // Calculate the number of task, completed task, pendingTask
+  const handleCalculate =()=>{
+    setCompletedTask(0);
+    setTotalTask(0)
+    setPendingTask(0)
+    data.map((task)=>{
+      if (task.completed === true){
+        setCompletedTask(prev => prev+1);
+        setTotalTask(prev => prev+1)
+      }else{
+        setPendingTask(prev => prev+1)
+        setTotalTask(prev => prev+1)
+      }
+    })
+  }
+
+  // Fetch data from API
+  const fetchdata = async()=>{
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    setdata(data);
+  }
+
+  // function to add task
+  const handleAddTask = async()=>{
+    if (input.current.value === '') {
+      return;
+    }
+    let newData = {
+      "userId":1,
+      "id": new Date().getTime(),
+      "title": input.current.value,
+      "completed": false
+    };
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        body: JSON.stringify(newData),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      const addedTask = await response.json();
+      setdata((prevTasks) => [addedTask,...prevTasks]);
+      clearInput();
+    }catch (error) {
+      console.log('Error adding task:', error);
+    }
+
+  }
+
+  //function to delete task
+  const handleDelete = async(id)=>{
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'DELETE',
+      });
+      const deletedData = await response.json();
+      let newData = data.filter((task)=> task.id !== id);
+      setdata(newData);
+    }catch(error){
+      console.log('Error updating task:', error);
+    }
+  }
+
+  //function for checkbox to check task completed or not
+  const handleCheckbox = (id)=>{
+    setdata((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  }
+
+  //function to handle edit task function
+  const handleEdit= (id)=>{
+    setEdit(true);
+    let index = data.findIndex((task)=> task.id===id);
+    input.current.value = data[index].title;
+    setUpdateTaskData(data[index]);
+    setUpdateTaskId(id);
+  }
+
+  // function to update the value of task
+  const handleUpdateTask = async(id,newvalue)=>{
+    if(input.current.value=""){
+      console.log("enter some value please");
+      return;
+    }
+    const updatedTask = {...updatetaskdata,title:newvalue};
+
+    //Temperory function for the task which we add, Because we cant have access to make change in API server.
+    if(updatetaskdata.id > 200){
+      setdata((prevTasks)=> prevTasks.map((task)=> task.id===id ? {...task,title:newvalue}:task));
+      clearInput();
+      setEdit(false);
+      return;
+    }
+
+    //main logic, we dont require the above function, if we have access to make the change in server data
+    try {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(updatedTask),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      const updatedTaskData = await response.json();
+      setdata((prevTasks)=> prevTasks.map((task)=> task.id===id ? {...task,title:updatedTaskData.title}:task));
+      clearInput();
+      setEdit(false);
+    } catch (error) {
+      console.log('Error updating task:', error);
+    }
+  }
+
+  //function to clear input value
+  const clearInput = ()=>{
+    input.current.value='';
+  }
+
   return (
     <div className="App">
-      <h1>hello</h1>;
-      <Tasklist />
+      <h1>TodoList</h1> 
+      <div className='search-container'>
+        <div className='search-bar'><input type="text" ref={input} placeholder='Add a task' autoFocus/>
+        {isEdit?<button className='update' onClick={()=>handleUpdateTask(updatetaskId,input.current.value)}>Update</button>:<button className='add' onClick={handleAddTask}>Add</button>}
+        </div>
+        <div className='filter'>
+          <span className={filter==="All" && "active"} onClick={()=> setFilter("All")}>Totaltask:{totalTask} </span>
+          <span className={filter==="Complete" && "active"} onClick={()=> setFilter("Complete")}>Completed: {completedTask}</span>
+          <span className={filter==="pending" && "active"} onClick={()=> setFilter("pending")}>Pending: {pendingTask}</span>
+          </div>
+      </div>
+      <div className='task-container'>
+        <Tasklist data={data} 
+                  isEdit={isEdit} 
+                  filter={filter}
+                  handleDelete={handleDelete}
+                  handleCheckbox={handleCheckbox}
+                  handleEdit={handleEdit}/>
+      </div>
     </div>
   );
 }
--- my-app/src/pages/tasklist.js
+++ my-app/src/pages/tasklist.js
@@ -1,11 +1,40 @@
-import { useSelector } from "react-redux";
-import { todoSelector } from "../features/counter/todoSlice";
-
-export function Tasklist(){
-    const data = useSelector(todoSelector)
+import styled from "styled-components";
+import "./tasklist.css";
+import { RiDeleteBin6Fill } from "react-icons/ri";
+import { AiFillEdit } from "react-icons/ai";
+const TaskFont = styled.span`
+text-decoration: ${props => props.completed?"line-through 2px red":"none"};
+font-size: 1rem;
+`;
+export function Tasklist({data,handleDelete,handleCheckbox,updatetaskId,isEdit,handleEdit,filter}){
     return(<>
     <div className="container">
-        {data}
+        {data.length >0 && filter === "All"? data.map((task,i)=>{
+        return <div style={{display:"flex"}} className="task" key={i}>
+            <div><input type="checkbox" onClick={()=>handleCheckbox(task.id)} checked={task.completed}/></div>
+            <div className="task-title"><TaskFont completed={task.completed}>{task.title}</TaskFont></div>
+            <div className="edit-delete">
+                <span onClick={()=> handleEdit(task.id)}><AiFillEdit /></span>
+                <span onClick={()=> handleDelete(task.id)}><RiDeleteBin6Fill/></span>
+            </div>
+        </div>})
+        :data.length >0 && filter === "pending"? data.map((task,i)=> !task.completed && <div style={{display:"flex"}} className="task" key={i}>
+            <div><input type="checkbox" onClick={()=>handleCheckbox(task.id)} checked={task.completed}/></div>
+            <div className="task-title"><TaskFont completed={task.completed}>{task.title}</TaskFont></div>
+            <div className="edit-delete">
+                <span onClick={()=> handleEdit(task.id)}><AiFillEdit /></span>
+                <span onClick={()=> handleDelete(task.id)}><RiDeleteBin6Fill/></span>
+            </div>
+        </div>)
+        :data.length>0 && filter === "Complete"? data.map((task,i)=> task.completed && <div style={{display:"flex"}} className="task" key={i}>
+            <div><input type="checkbox" onClick={()=>handleCheckbox(task.id)} checked={task.completed}/></div>
+            <div className="task-title"><TaskFont completed={task.completed}>{task.title}</TaskFont></div>
+            <div className="edit-delete">
+                <span onClick={()=> handleEdit(task.id)}><AiFillEdit /></span>
+                <span onClick={()=> handleDelete(task.id)}><RiDeleteBin6Fill/></span>
+            </div>
+        </div>)
+        :null}
     </div>
     </>);
 }
